e8f09e252ea75904b505fc9c9b6c56867d71b885
Ke <ke.li@epfl.ch>
2023-05-28 16:07:04 +0200

    add a note

--- CLIP/predict.py
+++ CLIP/predict.py
@@ -80,6 +80,7 @@ if __name__ == "__main__":
               'Kimono', 'Suit', 'Gown', 'Glove']
     cifar100 = CIFAR100(os.path.expanduser("~/.cache"), transform=preprocess, download=True)
     # combine the text prompts with the cifar100 labels, get the unique labels
+    # ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
     text_prompts = text_prompts + cifar100.classes
     text_prompts = list(set(text_prompts))
     # index to label dictionary
